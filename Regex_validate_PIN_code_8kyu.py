18a57c4b3f4039d54b4bc0ddc87bd00fae7c8495
Hernosepo <gellerhernan@hotmail.com>
2022-06-22 17:35:54 -0500

    Update Regex_validate_PIN_code_8kyu.py

--- Regex_validate_PIN_code_8kyu.py
+++ Regex_validate_PIN_code_8kyu.py
@@ -1,4 +1,9 @@
 import re
 def validate_pin(pin):
-    return False if re.findall("\D\w\s",pin) or len(pin) != 6 and len(pin) != 4 else True
+    return False if re.findall("\D",pin) or len(pin) != 6 and len(pin) != 4 else True
+
+
+
+print(validate_pin("12345 "))
+
 
