17d5c94823ea183aee568d030ff36e4a630f337d
Hernosepo <gellerhernan@hotmail.com>
2022-06-24 17:03:19 -0500

    Update Find_the_first_non_consecutive_number_8kyu.py
    Solved

--- Find_the_first_non_consecutive_number_8kyu.py
+++ Find_the_first_non_consecutive_number_8kyu.py
@@ -1,8 +1,11 @@
 # Find the first non-consecutive number
 
 def first_non_consecutive(arr):
-    x = [arr[n+1] for n in range(len(arr)-1) if arr[n+1] - arr[n] != 1]
-    return int(x)
+    i = 1
+    for x in arr:
+        if i < len(arr) and arr[i] - arr[i-1] != 1:
+            return arr[i]
+        i += 1
+    return None
 
-
-print(first_non_consecutive([4,5,6,7,8,9,11]))
+print(first_non_consecutive([4,6,7,8,9,11]))
